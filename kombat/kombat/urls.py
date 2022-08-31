f7919d9a21aeddc1ebf661b44e6405a2cefb6510
joaquinzuazo <jzuazociolfi@gmail.com>
2022-08-31 17:22:19 -0300

    refactor: delete unnecessary files

--- kombat/kombat/urls.py
+++ kombat/kombat/urls.py
@@ -20,5 +20,5 @@ from talana import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.test),
+    path("", views.combat),
 ]
--- kombat/talana/views.py
+++ kombat/talana/views.py
@@ -3,29 +3,28 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 def start_play(player1, player2):
-    movements_p1 = [len(x) for x in player1['movimientos']]
-    hits_p1 = [len(x) for x in player1['golpes']]
+    movements_p1 = [len(x) for x in player1.get_movements()]
+    hits_p1 = [len(x) for x in player1.get_hits()]
     sum_buttons_p1 = sum(movements_p1 + hits_p1)
-    movements_p2 = [len(x) for x in player2['movimientos']]
-    hits_p2 = [len(x) for x in player2['golpes']]
+    movements_p2 = [len(x) for x in player2.get_movements()]
+    hits_p2 = [len(x) for x in player2.get_hits()]
     sum_buttons_p2 = sum(movements_p2 + hits_p2)
+
+    start_p1 = (player1, player2)
+    start_p2 = (player2, player1)
     
-    start = 1 if sum_buttons_p1 < sum_buttons_p2 else 2
+    start = start_p1 if sum_buttons_p1 < sum_buttons_p2 else start_p2
     if sum_buttons_p1 == sum_buttons_p2:
-        start = 1 if movements_p1 < movements_p2 else 2
+        start = start_p1 if movements_p1 < movements_p2 else start_p2
         if movements_p1 == movements_p2:
-            start = 1 if hits_p1 < hits_p2 else 2
+            start = start_p1 if hits_p1 < hits_p2 else start_p2
             if hits_p1 == hits_p2:
-                start = 1
+                start = start_p1
     return start
 
 @api_view(["GET"])
 def combat(request):
-    player1 = request.data.get("player1")
-    player2 = request.data.get("player2")
-    if player1 and player2:
-        print(start_play(player1, player2))
-        return Response(status=status.HTTP_200_OK)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_200_OK)
+    # else:
+    #     return Response(status=status.HTTP_400_BAD_REQUEST)
 
